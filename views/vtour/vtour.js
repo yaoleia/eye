2d5ac59f6875f84625673b0abf5a7131ed635aa6
yao <18504228500@163.com>
2017-08-05 23:14:55 +0800

    1

--- views/vtour/vtour.js
+++ views/vtour/vtour.js
@@ -1,7 +1,7 @@
 myapp.controller("vtourCtrl",function($scope, $ionicModal){
     embedpano({
-        swf: "https://omjbzg6gs.bkt.clouddn.com/krpano/tour.swf",
-        xml: "https://omjbzg6gs.bkt.clouddn.com/krpano/tour.xml",
+        swf: "http://omjbzg6gs.bkt.clouddn.com/krpano/tour.swf",
+        xml: "./views/vtour/tour.xml",
         target: "pano",
         html5: "prefer",
         mobilescale: 1.0,
